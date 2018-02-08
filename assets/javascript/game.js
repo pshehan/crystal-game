a5c192cd191b3906022d35311f3b76398f6a7f97
p shehan <pshehan@ps-MacBook-Air.att.net>
2018-02-07 20:55:52 -0600

    init commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+<script type="text/javascript">
+
+$(document).ready(function () {
+
+
+
+
+//create list of four crysal buttons
+
+
+
+
+//create list of random target values
+
+
+
+//create a clickOn rule for each crystal button,  make the amount appear on the click and add to the score counter with each click
+
+
+
+//create an if/else for player's total score matching the random number, and reset the game if player reaches target or goes over
+
+
+
+//create new random value on each new game
+
+
+
+//create a counter of wins and losses
+
+
+
+}
+</script>
